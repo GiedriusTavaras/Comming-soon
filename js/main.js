04d896a33c405645b1d37aa613d9f49070ab6f42
GiedriusTavaras <tavaraz@gmail.com>
2020-07-25 17:43:02 +0300

    Mouse and barrs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import renderIcons from './renderIcons.js';
+import leftProgressBarss from '../data/progressBarss.js';
 import socials from '../data/socials.js';
 import renderClock from './/clock.js';
+import renderProgressBarrs from './renderProgresBar.js';
 
 renderIcons( '.icons-list', socials );
 renderClock( '.clock');
+
+renderProgressBarrs( '.column-left', leftProgressBarss);
+
